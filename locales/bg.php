3294a2df46ed6682632c9fa8a9b0a1edcdd44bf7
Ivo Petkov <ivo@ivopetkov.com>
2020-12-11 10:43:55 +0200

    Update Bulgarian texts

--- locales/bg.php
+++ locales/bg.php
@@ -10,7 +10,7 @@
 return [
     'bearcms.themes.mini.name' => 'Мини',
     'bearcms.themes.mini.description' => 'Минималистичен и изчистен дизайн, които дава възможност на вашето съдържание да изпъкне. Той е перфектен за мобилния свят, в който живеем. Дава възможност за добавяне на лого (или използване на текст) и предоставя инструменти за настройки на цветове, шрифтове, рамки и т.н.',
-    'bearcms.themes.mini.options.Header' => 'Хедър',
+    'bearcms.themes.mini.options.Header' => 'Хедър (горна част)',
     'bearcms.themes.mini.options.Logo' => 'Лого',
     'bearcms.themes.mini.options.Image' => 'Снимка',
     'bearcms.themes.mini.options.Text' => 'Текст',
@@ -24,7 +24,7 @@ return [
     'bearcms.themes.mini.options.Selected' => 'Избран елемент',
     'bearcms.themes.mini.options.Intro' => 'Приветствено съдържание (в началната страница)',
     'bearcms.themes.mini.options.Content' => 'Съдържание',
-    'bearcms.themes.mini.options.Footer' => 'Футър',
+    'bearcms.themes.mini.options.Footer' => 'Футър (долна част)',
     'bearcms.themes.mini.options.Window' => 'Прозорец',
     'bearcms.themes.mini.options.Elements' => 'Елементи',
     'bearcms.themes.mini.options.Pages' => 'Страници',
