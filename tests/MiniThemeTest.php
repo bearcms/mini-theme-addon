d80a0d3542520e782ab27917ece444f539b65375
Ivo Petkov <ivo@ivopetkov.com>
2021-12-03 13:16:57 +0200

    Add unit tests.

---
+++ tests/MiniThemeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Mini Theme addon for Bear CMS
+ * https://github.com/bearcms/mini-theme-addon
+ * Copyright (c) Amplilabs Ltd.
+ * Free to use under the MIT license.
+ */
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class MiniThemeTest extends BearCMS\AddonTests\PHPUnitTestCase
+{
+    /**
+     * 
+     */
+    public function testTheme()
+    {
+        $app = $this->getApp();
+
+        $this->assertTrue(true);
+    }
+}
